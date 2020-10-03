60883a4bff9cb562ad411a14b9820a45d9ac777d
bthomp22 <brentthompson2012@gmail.com>
2020-10-03 14:47:50 -0400

    first commit. saviongs account finished

--- src/main/java/com/meritamerica/assignment1/AccountHolder.java
+++ src/main/java/com/meritamerica/assignment1/AccountHolder.java
@@ -2,4 +2,88 @@ package com.meritamerica.assignment1;
 
 public class AccountHolder {
 	
+	//variables
+private String firstName;
+private String middleName;
+private String lastName;
+private String ssn;
+private double checkingAccount;
+private double savingsAccount;
+
+//default constructor
+	public AccountHolder(){
+	firstName = "";
+	middleName = "";
+	lastName = "";
+	ssn = "";
+	checkingAccount = 0.0;
+	savingsAccount = 0.0;
+		
+	}
+	
+	//constructor 
+	
+	public AccountHolder(String firstName, String middleName, String lastName, String ssn, double checkingAccountOpeningBalance, double savingsAccountOpeningBalance){
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName= lastName;
+		this.ssn = ssn;
+		this.checkingAccount = checkingAccountOpeningBalance;
+		this.savingsAccount = savingsAccountOpeningBalance;
+	}
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+		
+	}
+	
+	
+	public String getMiddleName() {
+		return middleName;
+	}
+	
+	public void middleName(String middleName) {
+		this.middleName = middleName;
+		
+	}
+	
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getSSN() {
+		return ssn;
+	}
+	
+	public void setSSN(String ssn) {
+		this.ssn = ssn;
+	}
+	public checkingAccount getCheckingAccount() {
+		return checkingAccount;
+	}
+	
+	public SavingsAccount getSavingsAccount() {
+		return savingsAccount;
+	}
+	
+	public String toString() {
+		String str = "1. Name: " + firstName + " " + middleName + " " + lastName + 
+					"\n2. SSN: " + ssn + 
+					"\n3. Checking Account Balance:" + checkingAccount + 
+					"\n4. Checking Account Interest Rate: " + Sa;
+		
+		
+		
+	}
+	
 }
--- src/main/java/com/meritamerica/assignment1/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment1/SavingsAccount.java
@@ -2,4 +2,44 @@ package com.meritamerica.assignment1;
 
 public class SavingsAccount {
 	
+	private double balance;
+	private double interestRate = .01;
+	
+	public SavingsAccount(double openingBalance) {
+		balance = openingBalance;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public double getInterestRate() {
+		return interestRate;
+	}
+	public boolean withdraw(double amount) {
+		if (amount <= balance) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public boolean deposit(double amount) {
+		if(amount>0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public double futureValue(int years) {
+		double futureV = balance * Math.pow((1 + interestRate), years);
+		return futureV;
+	}
+	
+	public String toString() {
+		String str = "1. Savings Account Balance: $" + balance + 
+				"\n2. Savings Account Interest Rate: " + interestRate + 
+				"\n3.Savings Account Balance in 3 years: $" + futureValue(3);
+		return str;
+	}
 }
